9b25f3eaf8616d62db15daeefd92ee8b73c2a654
Turner Hayes <dev@turnerhayes.com>
2024-03-06 14:05:09 -0500

    Refactor category chips into component

--- app/components/Pantry/Pantry.tsx
+++ app/components/Pantry/Pantry.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useCallback, useState } from "react";
-import { Chip, Input, InputAdornment, Stack } from "@mui/material";
+import { Chip, Input, InputAdornment, Stack, Typography } from "@mui/material";
 import { Search as SearchIcon } from "@mui/icons-material";
 
 
@@ -35,18 +35,52 @@ const CategoryChip = ({
     );
 };
 
+const CategoryChipList = ({
+    selectedCategory,
+    setSelectedCategory,
+}: {
+    selectedCategory: string|null;
+    setSelectedCategory: (category: string|null) => void;
+}) => {
+    const handleCategorySelected = useCallback((category: string|null) => {
+        setSelectedCategory(category);
+    }, [setSelectedCategory]);
+
+    return (
+        <Stack
+            direction="row"
+            spacing={1}
+            sx={{paddingTop: 1, paddingBottom: 1, width: "100%", overflow: "auto",}}
+        >
+            <CategoryChip
+                category={null}
+                isSelected={selectedCategory === null}
+                onSelected={handleCategorySelected}
+            />
+            {
+                CATEGORIES.map((category) => (
+                    <CategoryChip
+                        key={category}
+                        category={category}
+                        isSelected={selectedCategory === category}
+                        onSelected={handleCategorySelected}
+                    />
+                ))
+            }
+        </Stack>
+    );
+};
+
+
 export const Pantry = () => {
     const [selectedCategory, setSelectedCategory] = useState<string|null>(null);
     const [searchString, setSearchString] = useState("");
+    const [currentIngredients, setCurrentIngredients] = useState<string[]>([]);
 
     const handleSearchChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
         const newValue = event.target.value;
         setSearchString(newValue);
     }, [setSearchString]);
-
-    const handleCategorySelected = useCallback((category: string|null) => {
-        setSelectedCategory(category);
-    }, [setSelectedCategory]);
 
     return (
         <Stack>
@@ -61,27 +95,23 @@ export const Pantry = () => {
                 }
                 sx={{paddingTop: 1}}
             />
-            <Stack
-                direction="row"
-                spacing={1}
-                sx={{paddingTop: 1, paddingBottom: 1, width: "100%", overflow: "auto",}}
-            >
-                <CategoryChip
-                    category={null}
-                    isSelected={selectedCategory === null}
-                    onSelected={handleCategorySelected}
-                />
-                {
-                    CATEGORIES.map((category) => (
-                        <CategoryChip
-                            key={category}
-                            category={category}
-                            isSelected={selectedCategory === category}
-                            onSelected={handleCategorySelected}
-                        />
-                    ))
-                }
-            </Stack>
+            <CategoryChipList
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+            />
+            {
+                currentIngredients.length === 0 ? (
+                    <Stack>
+                        <Typography variant="h4" align="center" paddingTop={5}>
+                            Nothing to eat here yet...
+                        </Typography>
+                        <Typography variant="h6" align="center">
+                            Start adding ingredients from the categories above to
+                            generate recipes from our AI chef!
+                        </Typography>
+                    </Stack>
+                ) : null
+            }
         </Stack>
     )
 };
